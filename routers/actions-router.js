7d46dd6865af0059e932fedb1f71c8c07c37b6f3
LenWinkler <lenwinkler86@gmail.com>
2019-11-01 13:21:22 -0400

    added PUT and DELETE endpoints to actions-router

--- routers/actions-router.js
+++ routers/actions-router.js
@@ -22,6 +22,31 @@ router.get('/:id', (req, res) => {
             console.log('error getting action by ID', err);
             res.status(500).json({ errorMessage: "Unable to get action by ID" })
         })
+});
+
+router.put('/:id', (req, res) => {
+    Actions.update(req.params.id, req.body)
+        .then(response => {
+            if(response === null) {
+                res.status(404).json({ errorMessage: "Can't find action with that ID"})
+            } else {
+            res.status(200).json(response)
+        }})
+        .catch(err => {
+            console.log('error updating action', err);
+            res.status(500).json({ errorMessage: "Unable to update action" })
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    Actions.remove(req.params.id)
+        .then(response => {
+            res.status(200).json({ message: "Action deleted successfully" })
+        })
+        .catch(err => {
+            console.log('error deleting action', err);
+            res.status(500).json({ errorMessage: "Unable to delete action" })
+        })
 })
 
 module.exports = router;
